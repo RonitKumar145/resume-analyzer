dd1147ca6721074c47b7f411fc5e5285e3f9b760
RonitKumar145 <13ronitkumar@gmail.com>
2026-07-25 14:52:23 +0530

    updated for deployment

--- server/server.js
+++ server/server.js
@@ -12,7 +12,13 @@ connectDB()
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin:[
+        "http://localhost:5173",
+        process.env.CLIENT_URL,
+    ],
+    credentials:true,
+}))
 app.use(express.json())
 
 app.use("/api/resume", resumeRoutes)
